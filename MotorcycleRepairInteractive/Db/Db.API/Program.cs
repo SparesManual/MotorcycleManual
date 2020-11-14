95ad95ecd1351fb992bd1f40adb8beb9f44ec6e5
Denis Akopyan <denis.akopyan@seznam.cz>
2020-11-14 22:10:24 +0100

    #1 - Create the Db projects

---
+++ MotorcycleRepairInteractive/Db/Db.API/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace Db.API
+{
+  /// <summary>
+  /// Main API Class
+  /// </summary>
+  public static class Program
+  {
+    /// <summary>
+    /// Program entry point
+    /// </summary>
+    /// <param name="args">Console arguments</param>
+    public static void Main(string[] args)
+      => CreateHostBuilder(args)
+          .Build()
+          .Run();
+
+    /// <summary>
+    /// Creates the API host
+    /// </summary>
+    /// <param name="args">Supplied console arguments</param>
+    // ReSharper disable once MemberCanBePrivate.Global
+    public static IHostBuilder CreateHostBuilder(string[] args)
+      => Host
+          .CreateDefaultBuilder(args)
+          .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
+  }
+}
---
+++ MotorcycleRepairInteractive/Db/Db.API/Startup.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Db.API
+{
+  /// <summary>
+  /// API Client startup class
+  /// </summary>
+  public class Startup
+  {
+    /// <summary>
+    /// Configures the API server injected services
+    /// </summary>
+    /// <param name="services">Injected services library</param>
+    public void ConfigureServices(IServiceCollection services)
+      => services.AddGrpc();
+
+    /// <summary>
+    /// Configures the API server application functionality
+    /// </summary>
+    /// <param name="app">Application context</param>
+    /// <param name="env">Application hosting environment provider</param>
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+        app.UseDeveloperExceptionPage();
+
+      app.UseRouting();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapGrpcService<GreeterService>();
+
+        endpoints.MapGet("/", async context =>
+        {
+          await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+        });
+      });
+    }
+  }
+}
