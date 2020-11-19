a4593441384d09e35467f02d54fe500396f0118a
Denis Akopyan <denis.akopyan@seznam.cz>
2020-11-19 15:59:09 +0100

    Update seed data

---
+++ MotorcycleRepairInteractive/Db/Db.Infrastructure/Data/SeedCsvMapping/CsvSectionPartsMapping.cs
@@ -0,0 +1,20 @@
+using Db.Core.Entities;
+using TinyCsvParser.Mapping;
+
+namespace Db.Infrastructure.Data.SeedCsvMapping
+{
+  internal class CsvSectionPartsMapping
+    : CsvMapping<SectionParts>
+  {
+    public CsvSectionPartsMapping()
+    {
+      MapProperty(0, x => x.Id);
+      MapProperty(1, x => x.SectionId);
+      MapProperty(2, x => x.PartId);
+      MapProperty(4, x => x.PageNumber);
+      MapProperty(6, x => x.AdditionalInfo);
+      MapProperty(7, x => x.Quantity);
+      MapProperty(8, x => x.Remarks);
+    }
+  }
+}
