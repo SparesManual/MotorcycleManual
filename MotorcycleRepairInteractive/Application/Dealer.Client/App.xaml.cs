639ef5b74ddcec4b7acefc9747f4c023b1f9c258
Denis Akopyan <denis.akopyan@seznam.cz>
2021-02-20 01:47:24 +0100

    Add client application

---
+++ MotorcycleRepairInteractive/Application/Dealer.Client/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Autofac;
+using Components.Auth.WPF.Pages;
+using Db.Interfaces;
+using MRI.Auth;
+using MRI.MVVM.WPF.Helpers;
+using Validators.Auth;
+using ViewModels.Auth;
+using ViewModels.Interfaces.Auth.Validators;
+using ViewModels.Interfaces.Auth.ViewModels;
+
+namespace Dealer.Client
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App
+  {
+    public App()
+    {
+      NavigationManager.Register<LoginPage>("login");
+      NavigationManager.Register<RegisterPage>("register");
+
+      var builder = new ContainerBuilder();
+      builder.RegisterType<APIAuth>().As<IAPIAuth>();
+      builder.RegisterType<LoginValidator>().As<ILoginViewModelValidator>();
+      builder.RegisterType<RegisterValidator>().As<IRegisterViewModelValidator>();
+      builder.RegisterType<LoginViewModel>().As<ILoginViewModel>();
+      builder.RegisterType<RegisterViewModel>().As<IRegisterViewModel>();
+
+      var container = builder.Build();
+      ViewModelResolver.Initialize(container);
+    }
+  }
+}
---
+++ MotorcycleRepairInteractive/Libraries/Components/Components.Auth.WPF/Pages/RegisterPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using MRI.MVVM.Interfaces.Views;
+using MRI.MVVM.WPF.Helpers;
+using ViewModels.Interfaces.Auth.ViewModels;
+
+namespace Components.Auth.WPF.Pages
+{
+  /// <summary>
+  /// Interaction logic for RegisterPage.xaml
+  /// </summary>
+  public partial class RegisterPage
+    : IView<IRegisterViewModel>
+  {
+    /// <inheritdoc />
+    public IRegisterViewModel ViewModel { get; }
+
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public RegisterPage()
+    {
+      ViewModel = ViewModelResolver.Resolve<IRegisterViewModel>();
+      DataContext = ViewModel;
+
+      InitializeComponent();
+    }
+  }
+}
