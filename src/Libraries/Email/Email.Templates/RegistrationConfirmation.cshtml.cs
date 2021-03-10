d478cd801a031e7fad325e3266c4f1cc20dba3f7
Denis Akopyan <denis.akopyan@seznam.cz>
2021-03-10 17:23:12 +0100

    #25 - Add missing documentation

--- src/Libraries/Email/Email.Templates/RegistrationConfirmation.cshtml.cs
+++ src/Libraries/Email/Email.Templates/RegistrationConfirmation.cshtml.cs
@@ -1,7 +1,13 @@
 namespace Email.Templates
 {
+  /// <summary>
+  /// Registration confirmation model
+  /// </summary>
   public class RegistrationConfirmationModel
   {
+    /// <summary>
+    /// Account confirmation URL code
+    /// </summary>
     public string Code { get; set; } = string.Empty;
   }
 }
