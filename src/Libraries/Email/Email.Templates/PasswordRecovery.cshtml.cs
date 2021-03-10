98f80d51d5d809a5cfb010a6ffb140ab76634f6b
Denis Akopyan <denis.akopyan@seznam.cz>
2021-03-10 17:14:17 +0100

    #25 - Add the email templates library

---
+++ src/Libraries/Email/Email.Templates/PasswordRecovery.cshtml.cs
@@ -0,0 +1,7 @@
+namespace Email.Templates
+{
+  public class PasswordRecoveryModel
+  {
+    public string Code { get; set; } = string.Empty;
+  }
+}
---
+++ src/Libraries/Email/Email.Templates/RegistrationConfirmation.cshtml.cs
@@ -0,0 +1,7 @@
+namespace Email.Templates
+{
+  public class RegistrationConfirmationModel
+  {
+    public string Code { get; set; } = string.Empty;
+  }
+}
