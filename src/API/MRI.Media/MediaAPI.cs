816c2105276794f3b6ad40944436cfaab47b5d89
Denis Akopyan <denis.akopyan@seznam.cz>
2021-03-24 13:42:49 +0100

    #50 - Prepare Media API client

---
+++ src/API/MRI.Media/MediaAPI.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Media.Interfaces;
+
+namespace MRI.Media
+{
+  /// <summary>
+  /// Media providing API client
+  /// </summary>
+  public class MediaAPI
+    : IMediaAPI
+  {
+    /// <inheritdoc />
+    public ValueTask<string> GetUserProfileAsync(string email)
+    {
+      if (string.IsNullOrEmpty(email))
+        return new ValueTask<string>(string.Empty);
+
+      using var md5 = MD5.Create();
+      var inputBytes = Encoding.ASCII.GetBytes(email);
+      var hashBytes = md5.ComputeHash(inputBytes);
+
+      var builder = new StringBuilder();
+      foreach (var b in hashBytes)
+        builder.Append(b.ToString("X2"));
+
+      return new ValueTask<string>($"https://www.gravatar.com/avatar/{builder.ToString().ToLower()}?s={256}");
+    }
+
+    /// <inheritdoc />
+    public ValueTask<string> GetSectionImageAsync(int id) => throw new NotImplementedException();
+  }
+}
