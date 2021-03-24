c03fe08bfe177f1d6cc5da8a1573ef7fafbee70f
Denis Akopyan <denis.akopyan@seznam.cz>
2021-03-24 17:46:33 +0100

    #50 - Remove unused server endpoint

--- src/Libraries/Media/Media.API/Services/MediaService.cs
+++ src/Libraries/Media/Media.API/Services/MediaService.cs
@@ -1,8 +1,3 @@
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
-using Grpc.Core;
-
 namespace Media.API.Services
 {
   /// <summary>
@@ -12,27 +7,5 @@ namespace Media.API.Services
   public class MediaService
     : MediaProvider.MediaProviderBase
   {
-    /// <inheritdoc />
-    public override Task<StringMessage> GetUserProfile(StringMessage request, ServerCallContext context)
-    {
-      if (string.IsNullOrEmpty(request.Content))
-        return Task.FromResult(new StringMessage
-        {
-          Content = string.Empty
-        });
-
-      using var md5 = MD5.Create();
-      var inputBytes = Encoding.ASCII.GetBytes(request.Content);
-      var hashBytes = md5.ComputeHash(inputBytes);
-
-      var sb = new StringBuilder();
-      foreach (var b in hashBytes)
-        sb.Append(b.ToString("X2"));
-
-      return Task.FromResult(new StringMessage
-      {
-        Content = $"https://www.gravatar.com/avatar/{sb.ToString().ToLower()}?s={256}"
-      });
-    }
   }
 }
